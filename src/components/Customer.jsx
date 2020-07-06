28232e8338bededc6847613d2c8bbbdecec2b798
Lee <lsy63681@naver.com>
2020-07-06 18:05:25 +0900

    Update Customer

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,38 +1,21 @@
 import React, { PureComponent } from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 class Customer extends PureComponent {
   render() {
-    const { name, title, stack, db, id, image } = this.props;
+    const { name, current, account, state, id, image } = this.props;
     return (
-      <div>
-        <CustomerProfile img={image} id={id} name={name} />
-        <CustomerInfo title={title} stack={stack} db={db} />
-      </div>
-    );
-  }
-}
-class CustomerProfile extends PureComponent {
-  render() {
-    const { img, id, name } = this.props;
-    return (
-      <div>
-        <img src={img} alt="profile" />
-        <h2>
-          {name}({id})
-        </h2>
-      </div>
-    );
-  }
-}
-class CustomerInfo extends PureComponent {
-  render() {
-    const { title, stack, db } = this.props;
-    return (
-      <div>
-        <p>{title}</p>
-        <p>{stack}</p>
-        <p>{db}</p>
-      </div>
+      <TableRow>
+        <TableCell>{id}</TableCell>
+        <TableCell>
+          <img src={image} alt="profile" />
+        </TableCell>
+        <TableCell>{name}</TableCell>
+        <TableCell>{current}</TableCell>
+        <TableCell>{account}</TableCell>
+        <TableCell>{state}</TableCell>
+      </TableRow>
     );
   }
 }
