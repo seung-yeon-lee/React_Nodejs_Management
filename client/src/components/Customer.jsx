dce391ab5f9061723c257c97764ddffeb7013c7b
Lee <lsy63681@naver.com>
2020-07-07 20:53:50 +0900

    Create UI

--- client/src/components/Customer.jsx
+++ client/src/components/Customer.jsx
@@ -1,21 +1,37 @@
 import React, { PureComponent } from "react";
-import TableRow from "@material-ui/core/TableRow";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { TableRow } from "@material-ui/core";
 import TableCell from "@material-ui/core/TableCell";
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const StyledTableCell = withStyles(() => ({
+  // head tag와 body 태그의 내용에 따라 color 변경 부분
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
 class Customer extends PureComponent {
   render() {
     const { name, current, account, state, id, image } = this.props;
     return (
-      <TableRow>
-        <TableCell>{id}</TableCell>
-        <TableCell>
+      <StyledTableRow>
+        <StyledTableCell>{id}</StyledTableCell>
+        <StyledTableCell>
           <img src={image} alt="profile" />
-        </TableCell>
-        <TableCell>{name}</TableCell>
-        <TableCell>{current}</TableCell>
-        <TableCell>{account}</TableCell>
-        <TableCell>{state}</TableCell>
-      </TableRow>
+        </StyledTableCell>
+        <StyledTableCell>{name}</StyledTableCell>
+        <StyledTableCell>{current}</StyledTableCell>
+        <StyledTableCell>{account}</StyledTableCell>
+        <StyledTableCell>{state}</StyledTableCell>
+      </StyledTableRow>
     );
   }
 }
