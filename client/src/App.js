5b496f8c498f36b19bae952627f355c8df01eddf
Lee <lsy63681@naver.com>
2020-07-07 20:53:22 +0900

    Connect RDS_Mysql

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,52 @@
 import React, { PureComponent } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import TableBody from "@material-ui/core/TableBody";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
-const styles = () => ({
-  root: {
-    width: "100%",
-    marginTop: "10px",
-    overflowX: "auto", // x축으로 overflow가 발생 할 수 있도록
-  },
+const StyledTable = withStyles(() => ({
   table: {
     minWidth: 1080,
   },
-});
+}))(Table);
 
-class App extends PureComponent {
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+export default class App extends PureComponent {
   state = {
     customers: "",
+    progress: 0,
+  };
+
+  timer = () => {
+    setInterval(() => {
+      this.setState((state) => ({
+        progress: state.progress + 10,
+      }));
+    }, 200);
   };
   componentDidMount() {
+    this.timer();
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((err) => console.log(err));
@@ -37,40 +59,47 @@ class App extends PureComponent {
 
   render() {
     const { customers } = this.state;
-    const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
+      <TableContainer style={{ marginTop: "10px" }} component={Paper}>
+        <StyledTable>
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>상품명</TableCell>
-              <TableCell>최소가</TableCell>
-              <TableCell>최대가</TableCell>
-              <TableCell>보관상태</TableCell>
+              <StyledTableCell>번호</StyledTableCell>
+              <StyledTableCell>이미지</StyledTableCell>
+              <StyledTableCell>상품명</StyledTableCell>
+              <StyledTableCell>최소가</StyledTableCell>
+              <StyledTableCell>최대가</StyledTableCell>
+              <StyledTableCell>보관상태</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers
-              ? customers.map((v) => {
-                  return (
-                    <Customer
-                      key={v.name}
-                      id={v.id}
-                      name={v.name}
-                      current={v.current}
-                      account={v.account}
-                      state={v.state}
-                      image={v.image}
-                    />
-                  );
-                })
-              : ""}
+            {customers ? (
+              customers.map((v) => {
+                return (
+                  <Customer
+                    key={v.name}
+                    id={v.id}
+                    name={v.name}
+                    current={v.current}
+                    account={v.account}
+                    state={v.state}
+                    image={v.image}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress
+                    variant="static"
+                    value={this.state.progress}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
-        </Table>
-      </Paper>
+        </StyledTable>
+      </TableContainer>
     );
   }
 }
-export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,37 +1,28 @@
+const fs = require("fs"); // file read를 위한 라이브러리
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+db.connect();
 
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/3",
-      name: "ItemList1",
-      current: "1,000,000원",
-      account: "1,200,000원",
-      state: "상",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/1",
-      name: "ItemList2",
-      current: "235,123원",
-      account: "400,201원",
-      state: "중",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/2",
-      name: "ItemList3",
-      current: "589,539원",
-      account: "698,212원",
-      state: "하",
-    },
-  ]);
+  db.query("SELECT * FROM CUSTOMER", (err, rows, fileds) => {
+    if (err) console.log(err);
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
